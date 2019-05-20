bdebc86ac2a4195923e8c4784d866210aaa32d51
zspatter <localhost@zspatter.dev>
2019-05-20 14:10:43 -0400

    Converts internal representation from byte[] to BigInteger

--- base-conversion/BinaryNumber.java
+++ base-conversion/BinaryNumber.java
@@ -1,33 +1,26 @@
+import java.math.BigInteger;
+
 public class BinaryNumber extends Number
 {
-    private byte[] data;
-
     @Override
     public String toString()
     {
-        // how else can I read decimal byte values as binary?
-        StringBuilder binString = new StringBuilder("0b");
-        for (byte b : data)
-            binString.append(Integer.toBinaryString(b));
-
-        return binString.toString();
+        return super.data.toString(2);
     }
 
     protected static boolean isValid(String userString)
     {
         if (!userString.startsWith("0b"))
             return false;
+        char[] digits = userString.substring(2).toCharArray();
 
-        // why does it think for loop doesn't loop?
-        for (int i = 2; i < userString.length(); i++)
+        for (char ch : digits)
         {
-            char checkedDigit = userString.charAt(i);
-
-            if (checkedDigit == '0' || checkedDigit == '1')
+            if (ch == '0' || ch == '1')
                 break;
             else
             {
-                System.out.println("This input contains a digit that is invalid in binary." + checkedDigit);
+                System.out.println("This input contains a digit that is invalid in binary." + ch);
                 return false;
             }
         }
@@ -37,36 +30,6 @@ public class BinaryNumber extends Number
     @Override
     public void fromString(String userString)
     {
-        int arrLength = (int) (Math.ceil((userString.length() - 2) / 8.0));
-        data = new byte[arrLength];
-
-        data[0] = handleLeadingZeros(userString);
-        int index = 2 + ((userString.length() - 2) % 8);
-
-        for (int i = 1; i < data.length; i++)
-        {
-            data[i] = getByte(userString.substring(index, index + 8));
-            index += 8;
-        }
-    }
-
-    private byte handleLeadingZeros(String string)
-    {
-        int byteRemainder = (string.length() - 2) % 8;
-        String isolateFirstDigits = string.substring(2, (10 - byteRemainder));
-        int firstDigits = Integer.parseInt(isolateFirstDigits);
-
-        return (byte) firstDigits;
-    }
-
-
-
-    private byte getByte(String string)
-    {
-        // still can't handle bin bytes starting with 1
-        byte b = Byte.parseByte(string.substring(1), 2);
-        if (string.charAt(0) == '1')
-            b += 0b10000000;
-        return (byte)(b & 0xFF);
+        super.data = new BigInteger(userString.substring(2), 2);
     }
 }
