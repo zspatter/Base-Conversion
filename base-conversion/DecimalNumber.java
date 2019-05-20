9a1d14aae8a63eca1b04cd55e235ff80897a6d0e
zspatter <localhost@zspatter.dev>
2019-05-20 14:10:55 -0400

    Converts internal representation from byte[] to BigInteger

--- base-conversion/DecimalNumber.java
+++ base-conversion/DecimalNumber.java
@@ -1,19 +1,41 @@
+import java.math.BigInteger;
+
 public class DecimalNumber extends Number
 {
-    private byte[] data;
-
-
     // BigInteger modulo operation
+
 
     public static boolean isValid(String userString)
     {
+        if (!userString.startsWith("0d"))
+            return false;
+
+        char[] digits = userString.substring(2).toCharArray();
+
+        for (char ch : digits)
+        {
+            if (ch >= '0' && ch <= '9')
+                break;
+            else
+            {
+                System.out.println("This input contains a digit that is invalid in binary." + ch);
+                return false;
+            }
+        }
+
         return false;
     }
+
 
     @Override
     public void fromString(String userString)
     {
-
+        super.data = new BigInteger(userString.substring(2), 10);
     }
 
+    @Override
+    public String toString()
+    {
+        return super.data.toString(10);
+    }
 }
