600da7d07f738bfc65e15cd9090f867cd1222888
zspatter <localhost@zspatter.dev>
2019-05-20 17:12:34 -0400

    Adds class which continually gathers input from console and displays conversion of all 4 types

--- base-conversion/BaseConverter.java
+++ base-conversion/BaseConverter.java
@@ -1,5 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BaseConverter
 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        converterLoop();
+    }
+
+    public static void printMenu()
+    {
+        System.out.println("Enter a binary (0b), octal (0o), decimal (0d), or hexidecimal (0x) number with it's corresponding prefix:");
+    }
+
+    public static void converterLoop()
+    {
+        while (true)
+        {
+            printMenu();
+            String input = scanner.next();
+            if (input == "exit".toUpperCase())
+                break;
+
+            INumber iNumber = NumberFactory.getNumber(input);
+            Number number;
+
+            if (iNumber instanceof BinaryNumber)
+                number = (BinaryNumber) iNumber;
+            else if (iNumber instanceof OctalNumber)
+                number = (OctalNumber) iNumber;
+            else if (iNumber instanceof DecimalNumber)
+                number = (DecimalNumber) iNumber;
+            else if (iNumber instanceof HexNumber)
+                number = (HexNumber) iNumber;
+            else
+                continue;
+
+            Number.printConvertedValues(iNumber);
+        }
+    }
+
 }
