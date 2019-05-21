0563f26913288c4ebaf0bafc6571782d78ddbc47
zspatter <localhost@zspatter.dev>
2019-05-21 09:59:13 -0400

    Reformats output from  and adds emphasis with ANSI codes

--- base-conversion/Number.java
+++ base-conversion/Number.java
@@ -6,6 +6,10 @@ abstract public class Number implements INumber
 {
     protected BigInteger data;
 
+    private static final String ANSI_YELLOW = "\033[33m";
+    private static final String ANSI_RESET = "\033[0m";
+    private static final String PADDING = "";
+
     public abstract void fromString(String userString);
 
     public void fromNumber(INumber number)
@@ -23,7 +27,6 @@ abstract public class Number implements INumber
         this.data = data;
     }
 
-    // how do take an INumber object and convert bin byte[] to a string of each number class?
     public static void printConvertedValues(INumber iNumber)
     {
         BinaryNumber binaryNumber = new BinaryNumber();
@@ -36,10 +39,13 @@ abstract public class Number implements INumber
         hexNumber.fromNumber(iNumber);
 
         System.out.printf("\nConverted values:" +
-                        "\n\tBinary: %s" +
-                        "\n\tOctal: %s" +
-                        "\n\tDecimal: %s" +
-                        "\n\tHexadecimal: %s\n\n",
-                binaryNumber.toString(), octalNumber.toString(), decimalNumber.toString(), hexNumber.toString());
+                        "\n\tBinary: %7s%s%s%s" +
+                        "\n\tOctal: %8s%s%s%s" +
+                        "\n\tDecimal: %6s%s%s%s" +
+                        "\n\tHexadecimal: %2s%s%s%s\n\n",
+                PADDING, ANSI_YELLOW, binaryNumber.toString(), ANSI_RESET,
+                PADDING, ANSI_YELLOW, octalNumber.toString(), ANSI_RESET,
+                PADDING, ANSI_YELLOW, decimalNumber.toString(), ANSI_RESET,
+                PADDING, ANSI_YELLOW, hexNumber.toString(), ANSI_RESET);
     }
 }
